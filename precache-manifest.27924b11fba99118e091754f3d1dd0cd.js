0807a30f697b9373e01b13e0ba6ffbc8113fae21
mestrpra <mestry.prashant10@gmail.com>
2020-06-27 21:55:58 +0530

    Updates

---
+++ precache-manifest.27924b11fba99118e091754f3d1dd0cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c759ce988fea6d9146a0a38be2c5aa",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "091512d8a199a1c93987",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091512d8a199a1c93987",
+    "url": "/react-deploy/static/js/main.a6aba2ce.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
